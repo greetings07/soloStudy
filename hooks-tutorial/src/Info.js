31c1a38d4fc14056a6481722e014d61425344d50
ASJ <greetings07@naver.com>
2022-06-28 08:20:29 +0900

    sixth commit

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,30 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import useInputs from './useInputs';
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  useEffect(() => {
-    console.log('effect');
-    console.log(name);
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    };
-  }, []);
-
-  const onChangeName = e => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = e => {
-    setNickname(e.target.value);
-  };
+  const [state, onChange] = useInputs({
+    name: '',
+    nickname: ''
+  });
+  const { name, nickname } = state;
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name='name' value={name} onChange={onChange} />
+        <input name='nickname' value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
